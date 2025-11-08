e4d25874fb4be9eddbf5b8e60cda45b1af285f74
Sok Sothy <sokthy99999@gmail.com>
2025-11-08 10:10:56 +0700

    Fix: update endpoint from '/life' to '/devopslife' in app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
   res.send('Hello, GitHub Actions!');
 });
 
-app.get('/life', (req, res) => {
+app.get('/devopslife', (req, res) => {
   res.json({ status: 'OK', timestamp: new Date() });
 });
 
